2e235b0870dc326dc2c21377f26e618c5e59f256
Adi Fatkhurozi <ady.fatkhurozi@gmail.com>
2019-01-18 14:17:16 +0700

    Http Module

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
+const http = require('http')
 
-const Logger = require('./logger')
-const logger = new Logger()
-    
-logger.on('messageLogged',(args)=> console.log('Listener Called',args) )
+const server = http.createServer((req,res)=>{
+    if(req.url==='/'){
+        res.write('Hello World')
+        res.end()
+    }
+    if(req.url==='/api/courses'){
+        res.write(JSON.stringify([1,2,3,4]))
+        res.end()
+    }
+})
+// server.on('connection',(socket)=>console.log('New connection')) //Example function on server Emitter
+server.listen(3000)
 
-logger.log('ini Pesan')
+console.log('Listeng on Port 3000...')
--- logger.js
+++
@@ -1,9 +0,0 @@
-const EventEmitter = require('events');
-    
-class Logger extends EventEmitter{
-    log(msg){
-        console.log(msg)
-        this.emit("messageLogged",{id:1,url:'http://'});
-    }
-}
-module.exports = Logger
