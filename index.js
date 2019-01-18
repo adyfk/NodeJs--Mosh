c54ddbac5b181e2139d86dcc1bb32bb07accf805
Adi Fatkhurozi <ady.fatkhurozi@gmail.com>
2019-01-19 00:46:40 +0700

    Express & Nodemon

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-//Add Make Global Packge
-//npm adduser
-//npm login
-//npm publish
-//your Packge willbe Online
+const express = require('express')
+const app = express()
+
+// app.post()
+// app.put()
+// app.delete()
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+app.get('/api', (req, res) => {
+  res.send([1, 2, 3, 4, 5, 6, 8])
+})
+
+// app.listen(3000, () => console.log('Listening On board'))
+app.listen(3000)
