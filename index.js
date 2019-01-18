00ac80fc692755ba26d174fef53426c2607de145
Adi Fatkhurozi <ady.fatkhurozi@gmail.com>
2019-01-18 17:17:31 +0700

    Publish Package Online

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-var _ = require("underscore");
-
-//Core Module
-//File or Folder
-
-const result = _.contains([1, 2, 3], 3);
-console.log(result);
+//Add Make Global Packge
+//npm adduser
+//npm login
+//npm publish
+//your Packge willbe Online
