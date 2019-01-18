e996c1a420ae6028c0b0e02366a0e2eb58ea825b
Adi Fatkhurozi <ady.fatkhurozi@gmail.com>
2019-01-19 00:58:55 +0700

    Environment Variables

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
-
+const dotenv = require('dotenv')
+dotenv.config()
 // app.post()
 // app.put()
 // app.delete()
@@ -11,5 +12,5 @@ app.get('/api', (req, res) => {
   res.send([1, 2, 3, 4, 5, 6, 8])
 })
 
-// app.listen(3000, () => console.log('Listening On board'))
-app.listen(3000)
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(port))
