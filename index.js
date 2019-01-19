12e55159f72253a93a66a101144f7b8a028b41e0
Adi Fatkhurozi <ady.fatkhurozi@gmail.com>
2019-01-19 21:05:24 +0700

    Genres

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const helmet = require('helmet')
 const express = require('express')
 const app = express()
-const datas = require('./routes/data')
+const genres = require('./routes/genres')
 app.use(express.json())
 app.use(helmet())
 
 app.set('view engine', 'pug')
 app.set('views', './views')
 
-app.use('/api', datas)
+app.use('/genre', genres)
 
 app.get('/', (req, res) => {
   res.render('index', { title: 'First Page', message: 'Hello' })
--- routes/genres.js
+++ routes/genres.js
@@ -1,15 +1,16 @@
 const express = require('express')
 const router = express.Router()
 const Joi = require('joi')
-const datas = [
-  { id: 1, nama: 'Adi Fatkhurozi' },
-  { id: 2, nama: 'Haerani Lathifah' }
+const genres = [
+  { id: 1, nama: 'Action' },
+  { id: 2, nama: 'Fantasi' },
+  { id: 3, nama: 'Horror' }
 ]
 router.get('/', (req, res) => {
-  res.send(datas)
+  res.send(genres)
 })
 router.get('/:id', (req, res) => {
-  const result = datas.find(x => x.id === parseInt(req.params.id))
+  const result = genres.find(x => x.id === parseInt(req.params.id))
   if (!result) return res.status(404).send('Data tidak ada')
   res.send(result)
 })
@@ -17,28 +18,28 @@ router.post('/', (req, res) => {
   const { error } = vaidateData(req.body)
   if (error) return res.status(400).send(error.details[0].message)
   const data = {
-    id: datas.length + 1,
+    id: genres.length + 1,
     nama: req.body.nama
   }
-  datas.push(data)
-  res.send(datas)
+  genres.push(data)
+  res.send(genres)
 })
 
 router.put('/:id', (req, res) => {
-  const data = datas.find(x => x.id === parseInt(req.params.id))
+  const data = genres.find(x => x.id === parseInt(req.params.id))
   if (!data) return res.status(404).send('Data tidak ada')
   const { error } = vaidateData(req.body)
   if (error) return res.status(400).send(error.details[0].message)
   data.nama = req.body.nama
-  res.send(datas)
+  res.send(genres)
 })
 
 router.delete('/:id', (req, res) => {
-  const data = datas.find(x => x.id === parseInt(req.params.id))
+  const data = genres.find(x => x.id === parseInt(req.params.id))
   if (!data) return res.status(404).send('Data tidak ada')
-  const index = datas.indexOf(data)
-  datas.splice(index, 1)
-  res.send(datas)
+  const index = genres.indexOf(data)
+  genres.splice(index, 1)
+  res.send(genres)
 })
 
 module.exports = router
