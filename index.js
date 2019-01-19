ea546950af16fb198c01a884d4c05074ddb9702d
Adi Fatkhurozi <ady.fatkhurozi@gmail.com>
2019-01-19 17:08:28 +0700

    Templating Engines

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
-const startupDebugger = require('debug')('app:startup')
-const dbDebugger = require('debug')('app:db')
 const helmet = require('helmet')
 const express = require('express')
 const app = express()
 app.use(express.json())
 app.use(helmet())
 
-//to Show
-//add env debug
-//export DEBUG='app:startup'
-//export DEBUG='app:*'
-startupDebugger('Started')
-dbDebugger('Connected to Database')
+app.set('view engine', 'pug')
+app.set('views', './views')
 
+app.get('/', (req, res) => {
+  res.render('index', { title: 'First Page', message: 'Hello' })
+})
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(port))
