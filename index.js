c5462e46eeb1e7a86720a4d20b87c8a4ef01fc85
Adi Fatkhurozi <ady.fatkhurozi@gmail.com>
2019-01-19 23:02:13 +0700

    Promise

--- index.js
+++ index.js
@@ -1,32 +1,30 @@
 console.log('Before')
-getUser(1, getRepositories)
+getUser(1, user => {
+  getRepositories(user.gitHubUsername, repos => {
+    getCommits(repos[0], commits => {
+      console.log(commits)
+    })
+  })
+})
 console.log('After')
-
-function getRepositories(user) {
-  getRepositories(user.gitHubUsername, getCommits)
-}
-function getCommits(repos) {
-  getCommits(repo, displayCommits)
-}
-function displayRepositories(commits) {
-  console.log(commits)
-}
 
 function getUser(id, callback) {
   setTimeout(() => {
-    console.log('Reading a User from DB')
-    callback({ id: id, gitHubUsername: 'Adi' })
+    console.log('Reading a user from a database...')
+    callback({ id: id, gitHubUsername: 'mosh' })
   }, 2000)
 }
-function getRepositories(Username, callback) {
+
+function getRepositories(username, callback) {
   setTimeout(() => {
-    console.log('Calling Github API...')
-    callback(['repo1', 'repo2', 'repo2'])
+    console.log('Calling GitHub API...')
+    callback(['repo1', 'repo2', 'repo3'])
   }, 2000)
 }
-function getCommits(repos, callback) {
+
+function getCommits(repo, callback) {
   setTimeout(() => {
-    console.log('Calling Github API...')
-    callback(['1', '2', '2'])
+    console.log('Calling GitHub API...')
+    callback(['commit'])
   }, 2000)
 }
