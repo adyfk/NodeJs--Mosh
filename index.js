ac89fadf544fc832d4b7bef5364831236730ae4f
Adi Fatkhurozi <ady.fatkhurozi@gmail.com>
2019-01-20 13:09:12 +0700

    Query Document --CRUD

--- index.js
+++ index.js
@@ -17,14 +17,22 @@ const courseSchema = new mongoose.Schema({
 })
 
 const Course = mongoose.model('Course', courseSchema)
-async function createCourse() {
-  const course = new Course({
-    name: 'NodeJS',
-    author: 'Adi',
-    tags: ['Node', 'Backend'],
-    isPublished: true
-  })
-  const result = await course.save()
-  console.log(result)
+// async function createCourse() {
+//   const course = new Course({
+//     name: 'NodeJS',
+//     author: 'Adi',
+//     tags: ['Node', 'Backend'],
+//     isPublished: true
+//   })
+//   const result = await course.save()
+//   console.log(result)
+// }
+
+async function getCourses() {
+  const courses = await Course.find({ author: 'Adi', isPublished: true })
+    .limit(10)
+    .sort({ name: -1 }) //1 to asc / -1 to desc
+    .select({ name: 1, tags: 1 })
+  console.log(courses)
 }
-createCourse()
+getCourses()
