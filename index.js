435685a5bf74fe50106d94988bf1a96ef4d8afe4
Adi Fatkhurozi <ady.fatkhurozi@gmail.com>
2019-01-20 13:16:49 +0700

    Comparison Query Operator --CRUD

--- index.js
+++ index.js
@@ -29,7 +29,18 @@ const Course = mongoose.model('Course', courseSchema)
 // }
 
 async function getCourses() {
-  const courses = await Course.find({ author: 'Adi', isPublished: true })
+  //eq (equal)
+  //ne (not equal)
+  //gt (greater than)
+  //gte (greater than or equal to)
+  //lt (less than)
+  //lte (less than or equal to)
+  //in
+  //nin (not ini)
+  const courses = await Course
+    // .find({ author: 'Adi', isPublished: true })
+    //.find({ price: { $gt: 10, $lte: 20 } }) //gt (greater than)
+    .find({ price: { $in: [10, 15, 20] } }) //use or
     .limit(10)
     .sort({ name: -1 }) //1 to asc / -1 to desc
     .select({ name: 1, tags: 1 })
