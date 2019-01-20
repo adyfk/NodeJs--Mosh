ece0cd673b074dc921ee5f1a12b2ff0d82818d1a
Adi Fatkhurozi <ady.fatkhurozi@gmail.com>
2019-01-20 12:44:56 +0700

    Connect to MongoDB --CRUD

--- index.js
+++ index.js
@@ -1,60 +1,9 @@
-console.log('Before')
-// getUser(1, user => {
-//   getRepositories(user.gitHubUsername, repos => {
-//     getCommits(repos[0], commits => {
-//       console.log(commits)
-//     })
-//   })
-// })
+const mongoose = require('mongoose')
 
-//Promise Based
-// const p = getUser(1)
-//   .then(user => getRepositories(user.gitHubUsername))
-//   .then(repos => getCommits(repos[0]))
-//   .then(commits => console.log(commits))
-//   .catch(err => console.log('Error', err))
-// console.log('After')
-
-//Async and await
-async function displayCommits() {
-  try {
-    const user = await getUser(1)
-    console.log('hai')
-    const repo = await getRepositories(user.gitHubUsername)
-    console.log('hai')
-    const commits = await getCommits(repo[0])
-    console.log('hai')
-    console.log(commits)
-  } catch (err) {
-    console.log(err)
-  }
-}
-displayCommits() //Async will be skip
-console.log('after')
-
-function getUser(id) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      console.log('Reading a user from a database...')
-      resolve({ id: id, gitHubUsername: 'mosh' })
-    }, 2000)
-  })
-}
-
-function getRepositories(username) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      console.log('Calling GitHub API...')
-      resolve(['repo1', 'repo2', 'repo3'])
-    }, 2000)
-  })
-}
-
-function getCommits(repo) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      console.log('Calling GitHub API...')
-      resolve(['commit'])
-    }, 2000)
-  })
-}
+mongoose
+  .connect(
+    'mongodb://localhost/playground',
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.log('ERROR =>', err))
