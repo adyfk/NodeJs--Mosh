2ea2454f958d13144748a441fa50b699e7e40cd5
Adi Fatkhurozi <ady.fatkhurozi@gmail.com>
2019-01-21 13:18:50 +0700

    Project-Build The Costumers Api

--- vidly/index.js
+++ vidly/index.js
@@ -1,18 +1,16 @@
-const mongoose = require('mongoose')
-const genres = require('./routes/genres')
-const express = require('express')
-const app = express()
+const mongoose = require('mongoose');
+const genres = require('./routes/genres');
+const customers = require('./routes/customers');
+const express = require('express');
+const app = express();
 
-mongoose
-  .connect(
-    'mongodb://localhost/vidly',
-    { useNewUrlParser: true }
-  )
-  .then(() => console.log('Connected Mongo'))
-  .catch(err => console.error('Cannot Connect'))
+mongoose.connect('mongodb://localhost/vidly')
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.error('Could not connect to MongoDB...'));
 
-app.use(express.json())
-app.use('/api/genres', genres)
+app.use(express.json());
+app.use('/api/genres', genres);
+app.use('/api/customers', customers);
 
-const port = process.env.PORT || 3000
-app.listen(port, () => console.log(`Listening on port ${port}...`))
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
