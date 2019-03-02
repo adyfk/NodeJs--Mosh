d4558f2c1ea59c3ffcd7f3a26e5425b68536b249
Adi Fatkhurozi <adyfk.github.com>
2019-03-02 15:58:17 +0700

    Hashing Password Bcrypt

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const { User, validate } = require("../models/user");
 const express = require("express");
 const router = express.Router();
 const _ = require("lodash");
+const bcrypt = require("bcrypt");
 router.post("/", async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
@@ -10,7 +11,10 @@ router.post("/", async (req, res) => {
   if (user) return res.status(400).send("User Already registered");
 
   user = new User(_.pick(req.body, ["name", "email", "password"]));
-  await user.save().catch(err => res.status("400").send("Mongo Error"));
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
+
+  await user.save().catch(() => res.status("400").send("Mongo Error"));
 
   res.send(_.pick(user, ["_id", "name", "email"]));
 });
