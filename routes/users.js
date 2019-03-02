69ece5c99ad785bc716d02c5d61dbb18f0d668e6
Adi Fatkhurozi <adyfk.github.com>
2019-03-02 14:52:43 +0700

    Using Lodash (Easy get Object)

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const { User, validate } = require("../models/user");
 const express = require("express");
 const router = express.Router();
-
+const _ = require("lodash");
 router.post("/", async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
@@ -9,14 +9,24 @@ router.post("/", async (req, res) => {
   let user = await User.findOne({ email: req.body.email });
   if (user) return res.status(400).send("User Already registered");
 
-  user = new User({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password
-  });
+  //   user = new User({
+  //     name: req.body.name,
+  //     email: req.body.email,
+  //     password: req.body.password
+  //   });
+
+  // USing Lodash
+  user = new User(_.pick(req.body, ["name", "email", "password"]));
   await user.save().catch(err => res.status("400").send("Mongo Error"));
 
-  res.send(user);
+  //   res.send({
+  //     name: user.name,
+  //     email: user.email
+  //   });
+
+  //USing LODASH
+
+  res.send(_.pick(user, ["_id", "name", "email"]));
 });
 
 module.exports = router;
