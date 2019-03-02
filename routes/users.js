8864587b951f50dadf21561370242e67713f9d83
Adi Fatkhurozi <adyfk.github.com>
2019-03-03 00:18:03 +0700

    Setting Response Headers

--- routes/users.js
+++ routes/users.js
@@ -1,22 +1,26 @@
-const { User, validate } = require("../models/user");
-const express = require("express");
-const router = express.Router();
-const _ = require("lodash");
-const bcrypt = require("bcrypt");
-router.post("/", async (req, res) => {
-  const { error } = validate(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
+const { User, validate } = require('../models/user')
+const express = require('express')
+const router = express.Router()
+const _ = require('lodash')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const config = require('config')
+router.post('/', async (req, res) => {
+  const { error } = validate(req.body)
+  if (error) return res.status(400).send(error.details[0].message)
 
-  let user = await User.findOne({ email: req.body.email });
-  if (user) return res.status(400).send("User Already registered");
+  let user = await User.findOne({ email: req.body.email })
+  if (user) return res.status(400).send('User Already registered')
 
-  user = new User(_.pick(req.body, ["name", "email", "password"]));
-  const salt = await bcrypt.genSalt(10);
-  user.password = await bcrypt.hash(user.password, salt);
+  user = new User(_.pick(req.body, ['name', 'email', 'password']))
+  const salt = await bcrypt.genSalt(10)
+  user.password = await bcrypt.hash(user.password, salt)
 
-  await user.save().catch(() => res.status("400").send("Mongo Error"));
+  await user.save().catch(() => res.status('400').send('Mongo Error'))
 
-  res.send(_.pick(user, ["_id", "name", "email"]));
-});
+  const token = jwt.sign({ _id: user.id }, config.get('jwtPrivateKey'))
 
-module.exports = router;
+  res.header('x-auth-token', token).send(_.pick(user, ['_id', 'name', 'email']))
+})
+
+module.exports = router
