b875e60bdc2e84ebb7a1e6401885a900ae530058
Adi Fatkhurozi <adyfk.github.com>
2019-03-03 14:39:47 +0700

    Removing Try Catch Blocks

--- routes/genres.js
+++ routes/genres.js
@@ -4,26 +4,29 @@ const express = require('express')
 const router = express.Router()
 const auth = require('../middleware/auth')
 const admin = require('../middleware/admin')
+const asyncMiddleware = require('../middleware/async')
 
-router.get('/', async (req, res, next) => {
-  //if using async and await
-  try {
+router.get(
+  '/',
+  asyncMiddleware(async (req, res) => {
     const genres = await Genre.find().sort('name')
     res.send(genres)
-  } catch (ex) {
-    next(ex)
-  }
-})
+  })
+)
 
-router.post('/', auth, async (req, res) => {
-  const { error } = validate(req.body)
-  if (error) return res.status(400).send(error.details[0].message)
+router.post(
+  '/',
+  auth,
+  asyncMiddleware(async (req, res) => {
+    const { error } = validate(req.body)
+    if (error) return res.status(400).send(error.details[0].message)
 
-  let genre = new Genre({ name: req.body.name })
-  genre = await genre.save()
+    let genre = new Genre({ name: req.body.name })
+    genre = await genre.save()
 
-  res.send(genre)
-})
+    res.send(genre)
+  })
+)
 
 router.put('/:id', async (req, res) => {
   const { error } = validate(req.body)
