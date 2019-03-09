90b799e6c8169e9e4d80643ba516fd50a4240a65
Adi Fatkhurozi <adyfk.github.com>
2019-03-09 10:12:48 +0700

    Test-Driven Development Try

--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -10,28 +10,29 @@ describe('/api/genres', () => {
     server = require('../../index')
   })
   afterEach(async () => {
-    await Genre.remove({})
-    server.close()
+    await Genre.deleteMany()
+    await server.close()
   })
 
   describe('GET /', () => {
     it('should return all genres', async () => {
       const genres = [{ name: 'genre1' }, { name: 'genre2' }]
-      await Genre.collection.insertMany(genres, () => {
-        request(server).get('/api/genres', res => {
-          expect(res.status).toBe(200)
-          expect(res.body.length).toBe(2)
-          expect(res.body.some(g => g.name === 'genre1')).toBeTruthy()
-          expect(res.body.some(g => g.name === 'genre2')).toBeTruthy()
-        })
-      })
+
+      await Genre.collection.insertMany(genres)
+
+      const res = await request(server).get('/api/genres')
+
+      expect(res.status).toBe(200)
+      expect(res.body.length).toBe(2)
+      expect(res.body.some(g => g.name === 'genre1')).toBeTruthy()
+      expect(res.body.some(g => g.name === 'genre2')).toBeTruthy()
     })
   })
 
   describe('GET /:id', () => {
     it('should return a genre if valid id is passed', async () => {
       const genre = new Genre({ name: 'genre1' })
-      genre.save()
+      await genre.save()
 
       const res = await request(server).get('/api/genres/' + genre._id)
 
